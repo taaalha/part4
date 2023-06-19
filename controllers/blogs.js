06c0dbf81623b19f642035e391c7fa7cf962b4b2
Talha <talha.khalid@aalto.fi>
2023-06-19 20:43:19 +0300

    4.17 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,30 +1,42 @@
 const blogsRouter = require('express').Router()
 const Blog = require('./../models/blogs')
 const logger = require('../utils/logger')
+const User = require('../models/user')
 
 //Route 1
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+  .find({})
+  .populate('user',{username: 1, name: 1, id: 1})
+  response.json(blogs)
 })
  
 //Route 2
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  
+  //const user = await User.findById(body.userId)
+  const user = await User
+  .find({})
+  user_id = user[0].id
+
+  const fullUser = await User.findById(user_id)
+
+  console.log("user_id is" , user_id)
 
   const blog = new Blog(
     {
       title: body.title,
       author: body.author,
       url: body.url,
-      likes: body.likes || 0
+      likes: body.likes || 0,
+      user: user_id
     }
   )
   try {
     const savedBlog = await blog.save()
+    fullUser.blogs = fullUser.blogs.concat(savedBlog)  
+    await fullUser.save()
     response.status(201).json(savedBlog)
   }
   catch {
