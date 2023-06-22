a07b646fa6a094930e77516e8e4c706ebd568fca
Talha <talha.khalid@aalto.fi>
2023-06-22 23:01:08 +0300

    4.18 done

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ app.use(express.json())
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use('/api/blogs',blogsRouter)
 app.use('/api/users', usersRouter)
 app.use(middleware.errorHandler)
-
+app.use('/api/login', loginRouter)
 
 module.exports = app
